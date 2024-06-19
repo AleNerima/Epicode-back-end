6247bc6609a8acbd2a396da3632e79db28f93a4f
AleNerima <ale95polioni@gmail.com>
2024-06-19 14:48:36 +0200

    esercizio giorno tre

---
+++ settimana 1/giorno 3/esercizio giorno 3/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace EserciziCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scegli l'esercizio da eseguire:");
+            Console.WriteLine("1. Conto Corrente");
+            Console.WriteLine("2. Ricerca Nome");
+            Console.WriteLine("3. Somma e Media Array");
+            int scelta = int.Parse(Console.ReadLine());
+
+            switch (scelta)
+            {
+                case 1:
+                    ContoCorrente conto = new ContoCorrente();
+                    conto.MenuInizialeStart();
+                    break;
+                case 2:
+                    RicercaNome ricerca = new RicercaNome();
+                    ricerca.CercaNome();
+                    break;
+                case 3:
+                    SommaMediaArray sommaMedia = new SommaMediaArray();
+                    sommaMedia.CalcolaSommaMedia();
+                    break;
+                default:
+                    Console.WriteLine("Scelta non valida.");
+                    break;
+            }
+        }
+    }
+}
---
+++ settimana 1/giorno 3/esercizio giorno 3/ConsoleApp1/ricercanomi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class RicercaNome
+{
+    public void CercaNome()
+    {
+        string[] listanomi = { "francesco", "luigi", "mario", "giulia", "lorenzo" };
+        string nome;
+        int i;
+        bool verifica = false;
+
+        Console.WriteLine("Inserire il nome da ricercare");
+        nome = Console.ReadLine();
+
+        for (i = 0; i < listanomi.Length; i++)
+        {
+            if (listanomi[i] == nome)
+            {
+                Console.WriteLine("Il nome è nella lista");
+                verifica = true;
+            }
+        }
+        if (verifica == false)
+        {
+            Console.WriteLine("Il nome non è nella lista");
+        }
+
+        Console.ReadLine();
+    }
+}
