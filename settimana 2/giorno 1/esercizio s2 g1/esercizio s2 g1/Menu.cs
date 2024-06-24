9e6db0149292cd17a8bf237fccc2923a3c6567d4
AleNerima <ale95polioni@gmail.com>
2024-06-24 14:29:58 +0200

    esercizio giorno 1 settimana 2

---
+++ settimana 2/giorno 1/esercizio s2 g1/esercizio s2 g1/Menu.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleMenuApp
+{
+    public class Menu
+    {
+        public List<MenuItem> Items { get; set; }
+        public List<MenuItem> SelectedItems { get; set; }
+        public double ServiceCharge { get; set; }
+
+        public Menu()
+        {
+            Items = new List<MenuItem>
+            {
+                new MenuItem("Coca Cola 150 ml", 2.50),
+                new MenuItem("Insalata di pollo", 5.20),
+                new MenuItem("Pizza Margherita", 10.00),
+                new MenuItem("Pizza 4 Formaggi", 12.50),
+                new MenuItem("Pz patatine fritte", 3.50),
+                new MenuItem("Insalata di riso", 8.00),
+                new MenuItem("Frutta di stagione", 5.00),
+                new MenuItem("Pizza fritta", 5.00),
+                new MenuItem("Piadina vegetariana", 6.00),
+                new MenuItem("Panino Hamburger", 7.90)
+            };
+            SelectedItems = new List<MenuItem>();
+            ServiceCharge = 3.00;
+        }
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine("==============MENU==============");
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}:  {Items[i].Name} ({Items[i].Price:F2})");
+            }
+            Console.WriteLine("11: Stampa conto finale e conferma");
+            Console.WriteLine("==============MENU==============");
+        }
+
+        public void AddItem(int itemNumber)
+        {
+            if (itemNumber >= 1 && itemNumber <= Items.Count)
+            {
+                SelectedItems.Add(Items[itemNumber - 1]);
+                Console.WriteLine($"{Items[itemNumber - 1].Name} aggiunto al carrello.");
+            }
+            else
+            {
+                Console.WriteLine("Opzione non valida. Riprova.");
+            }
+        }
+
+        public void PrintReceipt()
+        {
+            Console.WriteLine("\n=============CONTO==============");
+            double total = 0;
+            foreach (var item in SelectedItems)
+            {
+                Console.WriteLine($"{item.Name} ({item.Price:F2})");
+                total += item.Price;
+            }
+            total += ServiceCharge;
+            Console.WriteLine($"Servizio al tavolo (€ {ServiceCharge:F2})");
+            Console.WriteLine($"Totale: \u20AC  {total:F2}");
+            Console.WriteLine("================================");
+        }
+
+        public void StartOrdering()
+        {
+            while (true)
+            {
+                DisplayMenu();
+                Console.Write("Seleziona un'opzione: ");
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    if (choice == 11)
+                    {
+                        PrintReceipt();
+                        break;
+                    }
+                    else
+                    {
+                        AddItem(choice);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Inserisci un numero valido.");
+                }
+            }
+        }
+    }
+}
---
+++ settimana 2/giorno 1/esercizio s2 g1/esercizio s2 g1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleMenuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.StartOrdering();
+        }
+    }
+}
+
+
